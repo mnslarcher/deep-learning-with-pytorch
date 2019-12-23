36cb4bdea86be0b6dac3ef03192c0a6ca377d5a8
Mario Namtao Shianti Larcher <24874992+mnslarcher@users.noreply.github.com>
2019-12-23 12:07:04 +0100

    Feature/p2ch7 (#15)
    * improved the filter for the master code folder
    
    * Completed dsets.py
    
    * Completed vis.py
    
    * renamed
    
    * Completed dsets and vis scriptspackages
    
    * Completed notebook to explore data
    
    * Complete disk, logconf and util packages

---
+++ util/disk.py
@@ -0,0 +1,82 @@
+import gzip 
+
+from diskcache import FanoutCache, Disk 
+from diskcache.core import BytesType, MODE_BINARY, BytesIO
+
+from util.logconf import logging
+log = logging.getLogger(__name__)
+log.setLevel(logging.INFO)
+
+class GzipDisk(Disk):
+    def store(self, value, read, key=None):
+        """
+        Override from base class diskcache.Disk.
+
+        Chunking is due to needing to work on pythons < 2.7.13:
+        - Issue #27130: In the "zlib" module, fix handling of large 
+            buffers (typically 2 or 4 GiB). Previously, inputs were
+            limited to 2 GiB, compression and decompression operations
+            did not properly handle results of 2 or 4 GiB.
+
+        :param value: value to convert
+        :param bool read: True when value is file-like object
+        :return: (size, mode, filename, value) tuple for Cache table
+        """
+        # pylint: disable=unidiomatic-typecheck
+        if type(value) is BytesType:
+            if read:
+                value = value.read()
+                read = False
+
+            str_io = BytesIO()
+            gz_file = gzip.GzipFile(mode='wb', compresslevel=1, fileobj=str_io)
+
+            for offset in range(0, len(value), 2 ** 30):
+                gz_file.write(value[offset:offset + 2 ** 30])
+            gz_file.close()
+
+            value = str_io.getvalue()
+
+        return super().store(value, read)
+
+    def fetch(self, mode, filename, value, read):
+        """
+        Override from base class diskcache.Disk.
+
+        Chunking is due to needing to work on pythons < 2.7.13:
+        - Issue #27130: In the "zlib" module, fix handling of large 
+            buffers (typically 2 or 4 GiB). Previously, inputs were
+            limited to 2 GiB, compression and decompression operations
+            did not properly handle results of 2 or 4 GiB.
+
+        :param int mode: value mode raw, binary, text or pickle
+        :param str filename: filename or corresponding value
+        :param value: database value
+        :param bool read: when True, return an open file handle
+        :return: corresponding Python value
+        """
+        value = super().fetch(mode, filename, value, read)
+
+        if mode == MODE_BINARY:
+            str_io = BytesIO(value)
+            gz_file = gzip.GzipFile(mode='rb', fileobj=str_io)
+            read_csio = BytesIO()
+
+            while True:
+                uncompressed_data = gz_file.read(2 ** 30)
+                if uncompressed_data:
+                    read_csio.write(uncompressed_data)
+                else:
+                    break 
+
+            value = read_csio.getvalue()
+
+        return value 
+
+
+def getCache(scope_str):
+    return FanoutCache("data-unversioned/cache/" + scope_str,
+                        disk=GzipDisk,
+                        shards=64,
+                        timeout=1,
+                        size_limit=2e11)
